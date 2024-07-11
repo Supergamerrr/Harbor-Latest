6ec43317cbd28e3211ee32c48709b6973693696f
Supergamerrr <83313652+Supergamerrr@users.noreply.github.com>
2024-07-12 01:27:29 +0300

    Update harbor.sh

--- harbor.sh
+++ harbor.sh
@@ -127,15 +127,15 @@ chmod +x start.sh
 ./start.sh
 #./lol.sh
 
-#$ROOTFS_DIR/usr/local/bin/proot \
-#--rootfs="${ROOTFS_DIR}" \
-#--link2symlink \
-#--kill-on-exit \
-#--root-id \
-#--cwd=/root \
-#--bind=/proc \
-#--bind=/dev \
-#--bind=/sys \
-#--bind=/tmp \
-#--bind=/var/run \
-#/bin/sh
+$ROOTFS_DIR/usr/local/bin/proot \
+--rootfs="${ROOTFS_DIR}" \
+--link2symlink \
+--kill-on-exit \
+--root-id \
+--cwd=/root \
+--bind=/proc \
+--bind=/dev \
+--bind=/sys \
+--bind=/tmp \
+--bind=/var/run \
+/bin/sh
